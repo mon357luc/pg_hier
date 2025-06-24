330001f88175e23a58cfb0f4f4a819ab9a4aa4d7
Sonny Wemhoener-Cuite <703111450@uniparks.com>
2025-06-24 18:57:31 -0400

    fixed hierify

--- extensions/serialized_fun_ext/sql/serialized_fun--0.1.sql
+++ extensions/serialized_fun_ext/sql/serialized_fun--0.1.sql
@@ -2,19 +2,18 @@
 CREATE FUNCTION hierify_sfunc(internal, VARIADIC "any")
 RETURNS internal
 AS 'MODULE_PATHNAME', 'hierify_sfunc'
-LANGUAGE C STRICT;
+LANGUAGE C IMMUTABLE;
 
 CREATE FUNCTION hierify_ffunc(internal)
 RETURNS text
 AS 'MODULE_PATHNAME', 'hierify_ffunc'
-LANGUAGE C STRICT;
+LANGUAGE C IMMUTABLE;
 
 CREATE AGGREGATE hierify(VARIADIC "any")
 (
     SFUNC     = hierify_sfunc,
     STYPE     = internal,
-    FINALFUNC = hierify_ffunc,
-    INITCOND  = NULL
+    FINALFUNC = hierify_ffunc
 );
 
 /* header helper */
