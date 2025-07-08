666b3ceadce334cc8636895312fb0bcf916fcd2b
Dupree <143921581+doitmann@users.noreply.github.com>
2025-07-08 18:28:05 -0400

    wokring on the ORM

--- extensions/pg_hier/src/pg_hier.c
+++ extensions/pg_hier/src/pg_hier.c
@@ -12,7 +12,7 @@ PG_FUNCTION_INFO_V1(pg_hier);
  * Relies on pg_hier_parse
  * and pg_hier_join.
  *
- * CREATE FUNCTION pg_hier(text)
+ * CREATE FUNCTION pg_hier(text, text DEFAULT 'auto')
  * RETURNS text
  * AS 'MODULE_PATHNAME', 'pg_hier'
  * LANGUAGE C STRICT;
@@ -24,6 +24,14 @@ Datum pg_hier(PG_FUNCTION_ARGS)
 
     text *input_text = PG_GETARG_TEXT_PP(0);
     char *input = text_to_cstring(input_text);
+    char *direction = NULL;
+    if (PG_NARGS() > 1 && !PG_ARGISNULL(1)) {
+        text *dir_text = PG_GETARG_TEXT_PP(1);
+        direction = text_to_cstring(dir_text);
+    } else {
+        direction = "auto";
+    }
+
     StringInfoData parse_buf;
     initStringInfo(&parse_buf);
 
@@ -49,40 +57,30 @@ Datum pg_hier(PG_FUNCTION_ARGS)
         }
     }
 
+    // Build join clause using new bidirectional join logic
+    char *parent_table = pstrdup(tables->data[0]);
+    char *child_table = pstrdup(tables->data[tables->size - 1]);
+
+    StringInfoData join_buf;
+    initStringInfo(&join_buf);
+
+    // Call pg_hier_join with direction
+    Datum join_result = DirectFunctionCall3(pg_hier_join,
+                                            CStringGetTextDatum(parent_table),
+                                            CStringGetTextDatum(child_table),
+                                            CStringGetTextDatum(cstring_to_text(direction)));
+    text *from_join_clause = DatumGetTextPP(join_result);
+    char *from_join_clause_str = text_to_cstring(from_join_clause);
+    appendStringInfoString(&join_buf, from_join_clause_str);
+
+    pfree(from_join_clause_str);
+    pfree(parent_table);
+    pfree(child_table);
+    appendStringInfoString(&parse_buf, " ");
+    appendStringInfoString(&parse_buf, join_buf.data);
+    pfree(join_buf.data);
+
     free_string_array(tables);
-
-    // int hier_id = pg_hier_find_hier(tables);
-
-    // char *parent_table = pstrdup(tables->data[0]);
-    // char *child_table = pstrdup(tables->data[tables->size - 1]);
-
-    // pfree(input);
-    // if (tables->data)
-    //     for (int i = 0; i < tables->size; ++i)
-    //     {
-    //         pfree(tables->data[i]);
-    //     }
-    // pfree(tables->data);
-    // pfree(tables);
-
-    // StringInfoData join_buf;
-    // initStringInfo(&join_buf);
-
-    // Datum join_result = DirectFunctionCall2(pg_hier_join,
-    //                                         CStringGetTextDatum(parent_table),
-    //                                         CStringGetTextDatum(child_table));
-    
-    // text *from_join_clause = DatumGetTextPP(join_result);
-    // char *from_join_clause_str = text_to_cstring(from_join_clause);
-    // appendStringInfoString(&join_buf, from_join_clause_str);
-
-    // pfree(from_join_clause_str);
-    // pfree(parent_table);
-    // pfree(child_table);
-    // appendStringInfoString(&parse_buf, " ");
-    // appendStringInfoString(&parse_buf, join_buf.data);
-    // pfree(join_buf.data);
-
     PG_RETURN_TEXT_P(cstring_to_text(parse_buf.data));
 }
 
@@ -132,7 +130,7 @@ Datum pg_hier_parse(PG_FUNCTION_ARGS)
 
 PG_FUNCTION_INFO_V1(pg_hier_join);
 /**************************************
- * CREATE FUNCTION pg_hier_join(TEXT, TEXT)
+ * CREATE FUNCTION pg_hier_join(TEXT, TEXT, TEXT)
  * RETURNS text
  * AS 'MODULE_PATHNAME', 'pg_hier_join'
  * LANGUAGE C STRICT;
@@ -143,17 +141,17 @@ Datum pg_hier_join(PG_FUNCTION_ARGS)
 
     text *parent_name_text = PG_GETARG_TEXT_PP(0);
     text *child_name_text = PG_GETARG_TEXT_PP(1);
-
+    text *direction_text = PG_NARGS() > 2 && !PG_ARGISNULL(2) ? PG_GETARG_TEXT_PP(2) : NULL;
     char *parent_name = text_to_cstring(parent_name_text);
     char *child_name = text_to_cstring(child_name_text);
+    char *direction = direction_text ? text_to_cstring(direction_text) : "auto";
 
-    Datum *name_path_elems;
-    bool *name_path_nulls;
-    int name_path_count;
-
-    Datum *key_path_elems;
-    bool *key_path_nulls;
-    int key_path_count;
+    Datum *name_path_elems = NULL;
+    bool *name_path_nulls = NULL;
+    int name_path_count = 0;
+    Datum *key_path_elems = NULL;
+    bool *key_path_nulls = NULL;
+    int key_path_count = 0;
 
     char *parent_table = NULL;
     char *join_table = NULL;
@@ -161,7 +159,6 @@ Datum pg_hier_join(PG_FUNCTION_ARGS)
     char *key_array = NULL;
     char *end_array = NULL;
     int key_idx;
-
     char *saveptr = NULL;
 
     StringInfoData join_sql;
@@ -171,43 +168,54 @@ Datum pg_hier_join(PG_FUNCTION_ARGS)
     if (ret != SPI_OK_CONNECT)
         elog(ERROR, "SPI_connect failed: %d", ret);
 
-    const char *fetch_hier =
+    // Bidirectional recursive CTE
+    const char *fetch_hier_down =
         "WITH RECURSIVE path AS ( "
-        "    SELECT "
-        "        id, name, parent_id, child_id, parent_key, child_key, "
-        "        ARRAY[name] AS name_path, "
-        "        COALESCE(pg_hier_make_key_step(parent_key, child_key), '{[]}') AS key_path, "
-        "        pg_typeof(pg_hier_make_key_step(parent_key, child_key)) AS ty1, "
-        "        pg_typeof('') AS ty2 "
-        "    FROM pg_hier_detail "
-        "    WHERE name = $1 "
+        "    SELECT id, name, parent_id, child_id, parent_key, child_key, ARRAY[name] AS name_path, COALESCE(pg_hier_make_key_step(parent_key, child_key), '{[]}') AS key_path "
+        "    FROM pg_hier_detail WHERE name = $1 "
         "    UNION ALL "
-        "    SELECT "
-        "        h.id, h.name, h.parent_id, h.child_id, h.parent_key, h.child_key, "
-        "        p.name_path || h.name, "
-        "        p.key_path || pg_hier_make_key_step(h.parent_key, h.child_key), "
-        "        pg_typeof(pg_hier_make_key_step(h.parent_key, h.child_key)), "
-        "        pg_typeof(p.key_path) "
-        "    FROM pg_hier_detail h "
-        "    JOIN path p ON h.parent_id = p.id "
-        ") "
-        "SELECT name_path, key_path "
-        "FROM path "
-        "WHERE name = $2;";
+        "    SELECT h.id, h.name, h.parent_id, h.child_id, h.parent_key, h.child_key, p.name_path || h.name, p.key_path || pg_hier_make_key_step(h.parent_key, h.child_key) "
+        "    FROM pg_hier_detail h JOIN path p ON h.parent_id = p.id "
+        ") SELECT name_path, key_path FROM path WHERE name = $2;";
 
+    const char *fetch_hier_up =
+        "WITH RECURSIVE path AS ( "
+        "    SELECT id, name, parent_id, child_id, parent_key, child_key, ARRAY[name] AS name_path, COALESCE(pg_hier_make_key_step(parent_key, child_key), '{[]}') AS key_path "
+        "    FROM pg_hier_detail WHERE name = $1 "
+        "    UNION ALL "
+        "    SELECT h.id, h.name, h.parent_id, h.child_id, h.parent_key, h.child_key, p.name_path || h.name, p.key_path || pg_hier_make_key_step(h.parent_key, h.child_key) "
+        "    FROM pg_hier_detail h JOIN path p ON h.child_id = p.id "
+        ") SELECT name_path, key_path FROM path WHERE name = $2;";
+
+    const char *fetch_hier = NULL;
     Oid argtypes[2] = {TEXTOID, TEXTOID};
     Datum values[2] = {PointerGetDatum(parent_name_text), PointerGetDatum(child_name_text)};
-    ret = SPI_execute_with_args(fetch_hier, 2, argtypes, values, NULL, true, 0);
-    if (ret != SPI_OK_SELECT)
-    {
-        SPI_finish();
-        elog(ERROR, "SPI_execute_with_args failed: %d", ret);
+    bool found = false;
+
+    if (strcmp(direction, "down") == 0) {
+        fetch_hier = fetch_hier_down;
+        ret = SPI_execute_with_args(fetch_hier, 2, argtypes, values, NULL, true, 0);
+        found = (ret == SPI_OK_SELECT && SPI_processed > 0);
+    } else if (strcmp(direction, "up") == 0) {
+        fetch_hier = fetch_hier_up;
+        ret = SPI_execute_with_args(fetch_hier, 2, argtypes, values, NULL, true, 0);
+        found = (ret == SPI_OK_SELECT && SPI_processed > 0);
+    } else {
+        // Try down first, then up
+        fetch_hier = fetch_hier_down;
+        ret = SPI_execute_with_args(fetch_hier, 2, argtypes, values, NULL, true, 0);
+        if (ret == SPI_OK_SELECT && SPI_processed > 0) {
+            found = true;
+        } else {
+            fetch_hier = fetch_hier_up;
+            ret = SPI_execute_with_args(fetch_hier, 2, argtypes, values, NULL, true, 0);
+            found = (ret == SPI_OK_SELECT && SPI_processed > 0);
+        }
     }
 
-    if (SPI_processed == 0)
-    {
+    if (!found) {
         SPI_finish();
-        elog(ERROR, "No path found from %s to %s", parent_name, child_name);
+        elog(ERROR, "No path found from %s to %s (direction: %s)", parent_name, child_name, direction);
     }
 
     HeapTuple tuple = SPI_tuptable->vals[0];
@@ -222,72 +230,58 @@ Datum pg_hier_join(PG_FUNCTION_ARGS)
     int name_path_len = ArrayGetNItems(ARR_NDIM(name_path_arr), ARR_DIMS(name_path_arr));
     int key_path_len = ArrayGetNItems(ARR_NDIM(key_path_arr), ARR_DIMS(key_path_arr));
 
-    if (name_path_len < 2)
-    {
+    if (name_path_len < 2) {
         SPI_finish();
-        elog(ERROR, "No path found from %s to %s", parent_name, child_name);
+        elog(ERROR, "No path found from %s to %s (direction: %s)", parent_name, child_name, direction);
     }
 
     deconstruct_array(name_path_arr, TEXTOID, -1, false, 'i',
                       &name_path_elems, &name_path_nulls, &name_path_count);
-
     deconstruct_array(key_path_arr, TEXTOID, -1, false, 'i',
                       &key_path_elems, &key_path_nulls, &key_path_count);
 
     resetStringInfo(&join_sql);
     appendStringInfo(&join_sql, "FROM %s", TextDatumGetCString(name_path_elems[0]));
 
-    for (int i = 1; i < name_path_count; i++)
-    {
+    for (int i = 1; i < name_path_count; i++) {
         parent_table = pstrdup(TextDatumGetCString(name_path_elems[i - 1]));
         join_table = pstrdup(TextDatumGetCString(name_path_elems[i]));
         key_json = TextDatumGetCString(key_path_elems[i]);
         key_array = key_json;
         while (*key_array && (*key_array == '[' || *key_array == ' '))
             key_array++;
-
         end_array = key_array + strlen(key_array) - 1;
-        while (end_array > key_array && (*end_array == ']' || *end_array == ' '))
-        {
+        while (end_array > key_array && (*end_array == ']' || *end_array == ' ')) {
             *end_array = '\0';
             end_array--;
         }
-
         char *pair = strtok_r(key_array, ",", &saveptr);
         appendStringInfo(&join_sql, " JOIN %s ON ", join_table);
         key_idx = 0;
-        while (pair)
-        {
+        while (pair) {
             while (*pair && (*pair == '"' || *pair == ' '))
                 pair++;
-
             char *end_quote = strchr(pair, '"');
             if (end_quote)
                 *end_quote = '\0';
-
             char *colon = strchr(pair, ':');
-            if (!colon)
-            {
+            if (!colon) {
                 SPI_finish();
                 elog(ERROR, "Invalid key format: %s", pair);
             }
             *colon = '\0';
             char *left_key = pair;
             char *right_key = colon + 1;
-
             if (key_idx > 0)
                 appendStringInfoString(&join_sql, " AND ");
-
-            elog(INFO, "Current table check 2: %s",
-                 TextDatumGetCString(name_path_elems[i]));
-
             appendStringInfo(&join_sql, "%s.%s = %s.%s",
                              parent_table, left_key,
                              join_table, right_key);
-
             key_idx++;
             pair = strtok_r(NULL, ",", &saveptr);
         }
+        pfree(parent_table);
+        pfree(join_table);
     }
     SPI_finish();
     PG_RETURN_TEXT_P(cstring_to_text(join_sql.data));
